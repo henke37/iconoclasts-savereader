8b3373c1c554b8b678ed061a3a69dd0dafd68d99
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-01-30 19:22:58 +0100

    Totally overkill automation to automatically hunt down the first savegame if none is specified.

--- Save reader/Program.cs
+++ Save reader/Program.cs
@@ -7,8 +7,19 @@ using System.Threading.Tasks;
 
 namespace Save_reader {
     class Program {
+        private const int APP_ID= 393520;
+
         static void Main(string[] args) {
-            var fs = File.OpenRead(args[0]);
+            if(args.Length == 1) {
+                DumpFile(args[0]);
+            } else {
+                string installPath=SteamHelper.GetInstallPathForApp(APP_ID);
+                DumpFile($@"{installPath}\data\save1");
+            }
+        }
+
+        private static void DumpFile(string fileName) {
+            var fs = File.OpenRead(fileName);
             var sf = new SaveFile(fs);
 
             foreach(var kv in sf.entries) {
--- Save reader/Properties/AssemblyInfo.cs
+++ Save reader/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Save reader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Henke37")]
 [assembly: AssemblyProduct("Save reader")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
